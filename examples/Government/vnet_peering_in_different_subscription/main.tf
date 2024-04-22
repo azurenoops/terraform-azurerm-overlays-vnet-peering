1fc7dcda5f68f9d0cdd45505bdb5135771e37be9
John Spinella <jrspinella@users.noreply.github.com>
2024-04-22 07:41:26 -0400

    Update examples

--- examples/Government/vnet_peering_in_different_subscription/main.tf
+++ examples/Government/vnet_peering_in_different_subscription/main.tf
@@ -13,14 +13,14 @@ module "mod_vnet_peerring" {
   org_name           = var.org_name
   workload_name      = var.workload_name
 
-  # Vnet Peerings details
-  enable_different_subscription_peering      = true
-  resource_group_1_name                      = azurerm_resource_group.vnet1-peering-network-rg.name
-  different_subscription_resource_group_name = azurerm_resource_group.vnet2-peering-network-rg.name
+    # Vnet Peerings details
+  enable_different_subscription_peering           = true
+  resource_group_src_name                         = azurerm_resource_group.vnet1-peering-network-rg.name
+  different_subscription_dest_resource_group_name = azurerm_resource_group.vnet2-peering-network-rg.name
 
-  alias_subscription_id            = "d8385cbe-d2c3-4042-b48c-d1d8bf6e5e06"
-  vnet_1_name                      = azurerm_virtual_network.vnet1-peering-vnet.name
-  vnet_1_id                        = azurerm_virtual_network.vnet1-peering-vnet.id
-  different_subscription_vnet_name = azurerm_virtual_network.vnet2-peering-vnet.name
-  different_subscription_vnet_id   = azurerm_virtual_network.vnet2-peering-vnet.id
+  alias_subscription_id                 = "d83xxxxxxxxxxxxxxxxxxxxx5e06"
+  vnet_src_name                         = azurerm_virtual_network.vnet1-peering-vnet.name
+  vnet_src_id                           = azurerm_virtual_network.vnet1-peering-vnet.id
+  different_subscription_dest_vnet_name = azurerm_virtual_network.vnet2-peering-vnet.name
+  different_subscription_dest_vnet_id   = azurerm_virtual_network.vnet2-peering-vnet.id
 }
--- examples/Government/vnet_peering_in_same_subscription/main.tf
+++ examples/Government/vnet_peering_in_same_subscription/main.tf
@@ -14,13 +14,13 @@ module "mod_vnet_peerring" {
   workload_name      = var.workload_name
 
   # Vnet Peerings details
-  enable_vnet_peering  = true
-  resource_group_1_name = azurerm_resource_group.vnet1-peering-network-rg.name
-  resource_group_2_name = azurerm_resource_group.vnet2-peering-network-rg.name
+  enable_vnet_peering      = true
+  resource_group_src_name  = azurerm_resource_group.vnet1-peering-network-rg.name
+  resource_group_dest_name = azurerm_resource_group.vnet2-peering-network-rg.name
 
   peer_to_different_resource_group = true
-  vnet_1_name                      = azurerm_virtual_network.vnet1-peering-vnet.name
-  vnet_1_id                        = azurerm_virtual_network.vnet1-peering-vnet.id
-  vnet_2_name                      = azurerm_virtual_network.vnet2-peering-vnet.name
-  vnet_2_id                        = azurerm_virtual_network.vnet2-peering-vnet.id
+  vnet_src_name                    = azurerm_virtual_network.vnet1-peering-vnet.name
+  vnet_src_id                      = azurerm_virtual_network.vnet1-peering-vnet.id
+  vnet_dest_name                   = azurerm_virtual_network.vnet2-peering-vnet.name
+  vnet_dest_id                     = azurerm_virtual_network.vnet2-peering-vnet.id
 }
